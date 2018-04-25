1ead8126bbda18522153fc9044d38a42bb31e7a6
Michael Ye <micxye@yahoo.com>
2018-04-24 17:12:39 -0700

    initial proxy server setup

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000 ;
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,17 +17,8 @@ app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
 
-// cors
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
-});
-
-app.get('/', (req, res, next) => {
-  // Handle the get for this route
-});
-
-app.post('/', (req, res, next) => {
-  // Handle the post for this route
 });
